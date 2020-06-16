b11aa37b5c5fba2f7549a48df66b632890170fcc
Glauber Jesus da Silva <glauberjds1@hotmail.com>
2020-06-16 20:29:06 -0300

    segundo commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+
+import api from './services/api'
+import './styles.css';
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    async function getRepositories() {
+      const response = await api.get('repositories');
+
+      setRepositories(response.data);
+    }
+    getRepositories();
+  }, []);
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+       title: `Novo Projeto ${Date.now()}`
+    });
+
+    setRepositories([...repositories, response.data])
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(repository => repository.id !== id));
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repository => (
+            <li key={repository.id}>
+             
+            <button onClick={() => handleRemoveRepository(repository.id)} >
+            {repository.title}
+          </button>
+          </li>
+        )
+        )}
+      </ul>
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
